2c10b165bd51595a085820e173f5a0269617afe1
beanZ9 <248049379+beanZ9@users.noreply.github.com>
2026-03-06 13:18:36 +0100

    refactor: performance, exception handling

--- StrongboxHighlight.cs
+++ StrongboxHighlight.cs
@@ -17,6 +17,7 @@ using System.Security.AccessControl;
 using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
+using ExileCore.Shared.Nodes;
 using Color = SharpDX.Color;
 
 namespace StrongboxHighlight;
@@ -25,6 +26,7 @@ public class StrongboxHighlight : BaseSettingsPlugin<StrongboxHighlightSettings>
 {
     private readonly List<string> _excludedStrings = new List<string>() { "account-bound", "italic" };
     private Dictionary<StrongboxHighlightEntry, List<Element>> _highlightedLabels = new Dictionary<StrongboxHighlightEntry, List<Element>>();
+    private List<LabelOnGround> _chestLabels = new List<LabelOnGround>();
     private SyncTask<bool> _currentTask;
     private bool _enabledArea;
     public override bool Initialise()
@@ -32,14 +34,14 @@ public class StrongboxHighlight : BaseSettingsPlugin<StrongboxHighlightSettings>
         if (!Settings.HighlightEntries.Any()) {
             Settings.HighlightEntries = new List<StrongboxHighlightEntry> {
                 new StrongboxHighlightEntry() {
-                    FrameColor = new Color(255, 0, 0, 100),
+                    FrameColor = new Color(255, 0, 0, 255),
                     BoxColor = new Color(255, 0, 0, 100),
                     DrawFrame = true,
                     DrawBox = true,
                     Regex = "detonates nearby corpses"
                 },
                 new StrongboxHighlightEntry() { 
-                    FrameColor = new Color(0, 255, 255, 100),
+                    FrameColor = new Color(0, 255, 255, 255),
                     BoxColor = new Color(0, 255, 255, 100),
                     DrawFrame = true,
                     DrawBox = true,
@@ -47,62 +49,80 @@ public class StrongboxHighlight : BaseSettingsPlugin<StrongboxHighlightSettings>
                 }
             };
         }
+
+        Settings.Reload.OnPressed += () => {
+
+        };
         return true;
     }
 
     public override void AreaChange(AreaInstance area)
     {
         _enabledArea = !area.IsHideout && !area.IsTown && !area.IsPeaceful;
+        if (_enabledArea) {
+            _currentTask = Update();
+        } else {
+            _currentTask = null;
+        }
     }
 
     public override Job Tick()
     {
-        GameController.MultiThreadManager.AddJob(Update, Name);
+        if (_currentTask != null) {
+            TaskUtils.RunOrRestart(ref _currentTask, () => null);
+        }
         return null;
     }
 
-    private void Update() {
-        if (!_enabledArea) return;
-
-        DebugWindow.LogMsg($"{base.Name}: Update");
-        var fetchedChests = GetChests(100);
-        ProcessChests(fetchedChests);
-
+    private async SyncTask<bool> Update() {
+        while (_enabledArea) {
+            if (_chestLabels.Count < 1) {
+                _chestLabels = FindChests(100);
+                _highlightedLabels.Clear();
+                
+            }
+            await ProcessChests();
+            await Task.Delay(50);
+            return true;
+        }
+        return false;
     }
 
-    public override void Render()
-    {
+    public override void Render() {
         foreach (var highlight in _highlightedLabels) {
-            if (highlight.Key.DrawFrame) {
-                foreach (var label in highlight.Value) {
-                    Graphics.DrawFrame(label.GetClientRectCache, highlight.Key.FrameColor, 5);
+            foreach (var label in highlight.Value) {
+                var rect = label.GetClientRect();
+                if (highlight.Key.DrawBox) {
+                    Graphics.DrawBox(rect, highlight.Key.BoxColor, 0);
                 }
-            }
-            if (highlight.Key.DrawBox) {
-                foreach (var label in highlight.Value) {
-                    Graphics.DrawBox(label.GetClientRectCache, highlight.Key.BoxColor, 0);
+                if (highlight.Key.DrawFrame) {
+                    Graphics.DrawFrame(rect, highlight.Key.FrameColor, 5);
                 }
             }
         }
     }
 
-    private List<LabelOnGround> GetChests(int radius) {
-        var chestLabels = new List<LabelOnGround>();
-        chestLabels = GameController.Game.IngameState.IngameUi.ItemsOnGroundLabelsVisible
-            .Where(x => x is not null 
+    private List<LabelOnGround> FindChests(int radius) {
+        var foundChests = GameController.Game.IngameState.IngameUi.ItemsOnGroundLabelsVisible
+            .Where(x => x is not null
                 && x.ItemOnGround != null && x.ItemOnGround.Metadata != null
                 && x.ItemOnGround.Metadata.Contains("Metadata/Chests/StrongBoxes") && !x.ItemOnGround.IsOpened && x.ItemOnGround.DistancePlayer <= radius)
             .ToList();
-        return chestLabels;
+        return foundChests;
     }
 
-    private void ProcessChests(List<LabelOnGround> chestLabels) {
-        var processedLabels = new Dictionary<StrongboxHighlightEntry, List<Element>>();
+    private async SyncTask<bool> ProcessChests() {        
         for (int i = 0; i < Settings.HighlightEntries.Count; i++) {
             var entry = Settings.HighlightEntries[i];
-            Regex regex = new Regex(entry.Regex, RegexOptions.IgnoreCase | RegexOptions.Compiled);
+            Regex regex;
+            try {
+                regex = new Regex(entry.Regex, RegexOptions.IgnoreCase | RegexOptions.Compiled);
+            } catch (Exception e) {
+                DebugWindow.LogError($"Can't compile regex: {e.Message}");
+                return false;
+            }
 
-            foreach (var chest in chestLabels) {
+            foreach (var chest in _chestLabels) {
                 if (chest.Label[0] != null && chest.Label[0][1] != null && chest.Label[0][1].Children
                     .Where(x => !string.IsNullOrEmpty(x.Text) && !_excludedStrings.Any(s => x.Text.Contains(s, StringComparison.OrdinalIgnoreCase)))
                     .Select(x => x.Text.ToLower())
@@ -111,17 +131,18 @@ public class StrongboxHighlight : BaseSettingsPlugin<StrongboxHighlightSettings>
                     string added = string.Join("\n", modStrings);
 
                     if (regex.IsMatch(added)) {
-                        if (processedLabels.TryGetValue(entry, out var labels)) {
+                        if (_highlightedLabels.TryGetValue(entry, out var labels)) {
                             labels.Add(chest.Label);
                         } else {
-                            processedLabels.Add(entry, new List<Element>() { chest.Label });
+                            _highlightedLabels.Add(entry, new List<Element>() { chest.Label });
                         }
                     }
                 }
             }
         }
-        _highlightedLabels.Clear();
-        _highlightedLabels = processedLabels;
+        _chestLabels.Clear();
+        await Task.Delay(1);
+        return true;
     }
 
     public override void DrawSettings() {
@@ -167,6 +188,7 @@ public class StrongboxHighlight : BaseSettingsPlugin<StrongboxHighlightSettings>
                 }
                 if (ImGui.Button($"Delete##{i}")) {
                     Settings.HighlightEntries.RemoveAt(i);
+                    return;
                 }
                 entry.Regex = regex;
                 entry.FrameColor = frameColor.ToSharpColor();
--- StrongboxHighlightSettings.cs
+++ StrongboxHighlightSettings.cs
@@ -9,6 +9,7 @@ public class StrongboxHighlightSettings : ISettings
 {
     //Mandatory setting to allow enabling/disabling your plugin
     public ToggleNode Enable { get; set; } = new ToggleNode(false);
+    public ButtonNode Reload { get; set; } = new ButtonNode();
 
     [IgnoreMenu]
     public List<StrongboxHighlightEntry> HighlightEntries { get; set; } = new List<StrongboxHighlightEntry>();
